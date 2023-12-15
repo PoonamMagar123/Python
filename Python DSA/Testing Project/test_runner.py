0bbe29ba6d7a7c138e88260ab3dc675d4606a186
Poonam Magar <102014652+PoonamMagar123@users.noreply.github.com>
2023-12-15 23:26:59 +0530

    Add files via upload

---
+++ Python DSA/Testing Project/test_runner.py
@@ -0,0 +1,6 @@
+# test_runner.py
+import unittest
+from tests.test_calculator import TestCalculator
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Python DSA/Testing Project/tests/test_calculator.py
@@ -0,0 +1,11 @@
+# test_calculator.py
+import unittest
+from src.calculator import add_numbers
+
+class TestCalculator(unittest.TestCase):
+    def test_add_numbers(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
